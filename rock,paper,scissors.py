bcdb11e49faca20cd19d0a41d355dbfb7575688b
David Yin <29613693+E137299@users.noreply.github.com>
2024-09-30 13:41:58 +0000

    Initial Commit

--- rock,paper,scissors.py
+++ rock,paper,scissors.py
@@ -4,7 +4,7 @@ import os
 ######### Function Definitions #########
 def print_instructions():
     print("\n\nWelcome to Rock, Paper, Scissors!\n")
-    print("Rock, Paper, Scissors is a simple hand game played between two people. Each player simultaneously chooses one of three options: rock, paper, or scissors.\nThe winner of each round is determined by the following rules:\n\n  - Rock (🪨) beats Scissors (✂️) because rock can crush scissors.\n  - Scissors (✂️) beats Paper (📄) because scissors can cut paper.\n  - Paper (📄) beats Rock (🪨) because paper can wrap around rock.\n\nIf both players choose the same option, the round is a tie. The game is typically played in multiple rounds, and the player with the most wins at the end is the overall winner.\n\n")
+    print("Rock, Paper, Scissors is a simple hand game played between two people. Each player simultaneously chooses one of three options: rock, paper, or scissors.\nThe winner of each round is determined by the following rules:\n\n  - Rock (🪨 ) beats Scissors (✂️  ) because rock can crush scissors.\n  - Scissors (✂️  ) beats Paper (📄 ) because scissors can cut paper.\n  - Paper (📄 ) beats Rock (🪨  ) because paper can wrap around rock.\n\nIf both players choose the same option, the round is a tie. The game is typically played in multiple rounds, and the player with the most wins at the end is the overall winner.\n\n")
 
 
 
